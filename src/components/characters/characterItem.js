3a8d3194938e04668651405067189ba37ac01d92
asimhyff <asimhyff@users.noreply.github.com>
2020-07-03 16:21:05 +0500

    updated

--- src/components/characters/characterItem.js
+++ src/components/characters/characterItem.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const characterItem = ({ item }) => {
     return (
-        <div  style={{ height: '18rem', width: '13rem', border: '2px solid white', padding: '1rem', borderRadius: '.5rem', justifyContent:'center'}}>
-            <section className="center"><img src={item.img} style={{height:'11rem' , width:'11rem'}}  /></section>
+        <div  style={{ height: '20rem', width: '15rem', border: '2px solid white', padding: '1rem', borderRadius: '.5rem', justifyContent:'center'}}>
+            <section className="center"><img src={item.img} style={{height:'13rem' , width:'13rem' , borderRadius: '.5rem' , marginBottom:'1rem'}}  /></section>
             <p><strong style={{paddingRight:'.5rem'}}>Name:</strong>{item.name}</p>
             <p><strong style={{paddingRight:'.5rem'}}>Nick Name:</strong>{item.nickname}</p>
             <p><strong style={{paddingRight:'.5rem'}}>Status:</strong>{item.status}</p>
