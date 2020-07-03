04ac31f5c4656f7b5cda9b19b914c0dd50bfe333
asimhyff <asimhyff@users.noreply.github.com>
2020-07-03 16:06:34 +0500

    updated

--- src/components/characters/charactergrid.js
+++ src/components/characters/charactergrid.js
@@ -4,11 +4,13 @@ import spinner from '../../img/spinner.gif'
 
 const charactergrid = ({ items , isLoading }) => {
     return isLoading ? <h1 className="center"><img src={spinner} style={{height:'5rem'}} /></h1> : (
+        <div className="center">
         <section className="cards">
             {
                 items.map(item => <CharacterItem key={item.char_id} item={item} />)
             }
         </section>
+        </div>
     )
 }
 
