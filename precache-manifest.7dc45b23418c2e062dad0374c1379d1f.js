62645f5e9a1f86183bc8ed096e15adcd6c08d1a0
asimhyff <asimhyff@users.noreply.github.com>
2020-07-03 15:19:12 +0500

    Updates

---
+++ precache-manifest.7dc45b23418c2e062dad0374c1379d1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e183ff4e0da6ed05d204e1fb8fd579",
+    "url": "/breakingbad-app/index.html"
+  },
+  {
+    "revision": "781dd61529ca3fc72827",
+    "url": "/breakingbad-app/static/css/main.2c7a14a2.chunk.css"
+  },
+  {
+    "revision": "a6b9df1d4b09d7daeed8",
+    "url": "/breakingbad-app/static/js/2.66619a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbad-app/static/js/2.66619a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781dd61529ca3fc72827",
+    "url": "/breakingbad-app/static/js/main.62593bd0.chunk.js"
+  },
+  {
+    "revision": "73b3e57eec86f6f12a7d",
+    "url": "/breakingbad-app/static/js/runtime-main.1ead4c64.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbad-app/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbad-app/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingbad-app/static/media/spinner.235b9b3e.gif"
+  }
+]);
